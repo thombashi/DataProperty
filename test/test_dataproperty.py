e6051cfe7f06d0fced73b38fe116926445255545
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-02-27 16:39:34 +0900

    try to increase coverage

--- test/test_dataproperty.py
+++ test/test_dataproperty.py
@@ -507,10 +507,26 @@ class Test_PropertyExtractor_extract_data_property_matrix:
         prop_matrix = PropertyExtractor.extract_data_property_matrix(value)
 
         assert len(prop_matrix) == 2
-        assert prop_matrix[0][0].typecode == Typecode.NONE
-        assert prop_matrix[0][1].typecode == Typecode.INT
-        assert prop_matrix[1][0].typecode == Typecode.FLOAT
-        assert prop_matrix[1][1].typecode == Typecode.STRING
+
+        prop = prop_matrix[0][0]
+        assert prop.typecode == Typecode.NONE
+        assert prop.align.align_code == Align.LEFT.align_code
+        assert prop.align.align_string == Align.LEFT.align_string
+
+        prop = prop_matrix[0][1]
+        assert prop.typecode == Typecode.INT
+        assert prop.align.align_code == Align.RIGHT.align_code
+        assert prop.align.align_string == Align.RIGHT.align_string
+
+        prop = prop_matrix[1][0]
+        assert prop.typecode == Typecode.FLOAT
+        assert prop.align.align_code == Align.RIGHT.align_code
+        assert prop.align.align_string == Align.RIGHT.align_string
+
+        prop = prop_matrix[1][1]
+        assert prop.typecode == Typecode.STRING
+        assert prop.align.align_code == Align.LEFT.align_code
+        assert prop.align.align_string == Align.LEFT.align_string
 
     @pytest.mark.parametrize(["value", "expected"], [
         [None, TypeError],
@@ -528,7 +544,7 @@ class Test_PropertyExtractor_extract_column_property_list:
             [
                 [1, 1.1, "aaa", 1,   1],
                 [2, 2.2, "bbb", 2.2, 2.2],
-                [3, 3.3, "ccc", 3,   "ccc"],
+                [3, 3.3, "ccc", -3,   "ccc"],
             ],
         ],
         [
@@ -536,7 +552,7 @@ class Test_PropertyExtractor_extract_column_property_list:
             [
                 [1, 1.1, "aaa", 1,   1],
                 [2, 2.2, "bbb", 2.2, 2.2],
-                [3, 3.3, "ccc", 3,   "ccc"],
+                [3, 3.3, "ccc", -3,   "ccc"],
             ],
         ],
         [
@@ -544,7 +560,7 @@ class Test_PropertyExtractor_extract_column_property_list:
             [
                 [1, 1.1, "aaa", 1,   1],
                 [2, 2.2, "bbb", 2.2, 2.2],
-                [3, 3.3, "ccc", 3,   "ccc"],
+                [3, 3.3, "ccc", -3,   "ccc"],
             ],
         ],
     ])
@@ -553,11 +569,31 @@ class Test_PropertyExtractor_extract_column_property_list:
             header_list, value)
 
         assert len(col_prop_list) == 5
-        assert col_prop_list[0].typecode == Typecode.INT
-        assert col_prop_list[1].typecode == Typecode.FLOAT
-        assert col_prop_list[2].typecode == Typecode.STRING
-        assert col_prop_list[3].typecode == Typecode.FLOAT
-        assert col_prop_list[4].typecode == Typecode.STRING
+
+        prop = col_prop_list[0]
+        assert prop.typecode == Typecode.INT
+        assert prop.align.align_code == Align.RIGHT.align_code
+        assert prop.align.align_string == Align.RIGHT.align_string
+
+        prop = col_prop_list[1]
+        assert prop.typecode == Typecode.FLOAT
+        assert prop.align.align_code == Align.RIGHT.align_code
+        assert prop.align.align_string == Align.RIGHT.align_string
+
+        prop = col_prop_list[2]
+        assert prop.typecode == Typecode.STRING
+        assert prop.align.align_code == Align.LEFT.align_code
+        assert prop.align.align_string == Align.LEFT.align_string
+
+        prop = col_prop_list[3]
+        assert prop.typecode == Typecode.FLOAT
+        assert prop.align.align_code == Align.RIGHT.align_code
+        assert prop.align.align_string == Align.RIGHT.align_string
+
+        prop = col_prop_list[4]
+        assert prop.typecode == Typecode.STRING
+        assert prop.align.align_code == Align.LEFT.align_code
+        assert prop.align.align_string == Align.LEFT.align_string
 
     @pytest.mark.parametrize(["header_list", "value", "expected"], [
         [
