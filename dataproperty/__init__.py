47e825fcf739fdebe5d69a37545a51c1f9ca786c
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-03-20 16:38:23 +0900

    Move MinMaxContainer class to the independent file.

--- dataproperty/__init__.py
+++ dataproperty/__init__.py
@@ -195,38 +195,6 @@ def _get_additional_format_len(data):
     return format_len
 
 
-class MinMaxContainer(object):
-
-    @property
-    def min_value(self):
-        return self.__min_value
-
-    @property
-    def max_value(self):
-        return self.__max_value
-
-    def __init__(self):
-        self.__min_value = None
-        self.__max_value = None
-
-    def diff(self):
-        return self.max_value - self.min_value
-
-    def average(self):
-        return (self.max_value + self.min_value) * 0.5
-
-    def update(self, value):
-        if self.__min_value is None:
-            self.__min_value = value
-        else:
-            self.__min_value = min(self.__min_value, value)
-
-        if self.__max_value is None:
-            self.__max_value = value
-        else:
-            self.__max_value = max(self.__max_value, value)
-
-
 class Typecode:
     NONE = 0
     INT = 1 << 0
