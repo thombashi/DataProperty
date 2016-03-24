75ec4b90fc2208664b9bda866458165db916e734
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-03-24 11:45:48 +0900

    Add a test

--- test/test_align_getter.py
+++ test/test_align_getter.py
@@ -6,7 +6,13 @@
 
 
 from dataproperty import *
+from dataproperty._align_getter import AlignGetter
 import pytest
+
+
+@pytest.fixture
+def align_getter():
+    return AlignGetter()
 
 
 class Test_AlignGetter_get_align_from_typecode:
@@ -17,5 +23,20 @@ class Test_AlignGetter_get_align_from_typecode:
         [Typecode.FLOAT, Align.RIGHT],
         [Typecode.NONE, Align.LEFT],
     ])
-    def test_normal(self, value, expected):
+    def test_normal(self, align_getter, value, expected):
+        assert align_getter.get_align_from_typecode(value) == expected
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [Typecode.STRING, Align.RIGHT],
+        [Typecode.INT, Align.LEFT],
+        [Typecode.FLOAT, Align.CENTER],
+        [Typecode.NONE, Align.LEFT],
+    ])
+    def test_setter(self, align_getter, value, expected):
+        align_getter.typecode_align_table = {
+            Typecode.STRING: Align.RIGHT,
+            Typecode.INT: Align.LEFT,
+            Typecode.FLOAT: Align.CENTER,
+        }
+
         assert align_getter.get_align_from_typecode(value) == expected
