81465f8c58fd8c05edf13cc5be826b9dec34b972
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-05-06 23:50:10 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ email = "gogogo.vm@gmail.com"
 
 setuptools.setup(
     name="DataProperty",
-    version="0.2.6",
+    version="0.2.7",
     url="https://github.com/thombashi/DataProperty",
     bugtrack_url="https://github.com/thombashi/DataProperty/issues",
 
