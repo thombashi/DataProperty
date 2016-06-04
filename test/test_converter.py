c62ace57c4a3975887e3f59ca12bdb18e613659c
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-06-04 15:17:15 +0900

    Move test cases

--- test/test_converter.py
+++ test/test_converter.py
@@ -7,9 +7,16 @@
 import datetime
 from dateutil.tz import tzoffset
 import pytest
+import six
 
 from dataproperty import DateTimeConverter
 from dataproperty import TypeConversionError
+from dataproperty import convert_value
+from dataproperty import is_nan
+
+
+nan = float("nan")
+inf = float("inf")
 
 
 class Test_DateTimeConverter_to_datetime:
@@ -65,3 +72,36 @@ class Test_DateTimeConverter_to_datetime:
 
         with pytest.raises(expected):
             dt_converter.to_datetime()
+
+
+class Test_convert_value:
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        ["0", 0],
+        [str(six.MAXSIZE), six.MAXSIZE],
+        [str(-six.MAXSIZE), -six.MAXSIZE],
+        [0, 0],
+        [six.MAXSIZE, six.MAXSIZE],
+        [-six.MAXSIZE, -six.MAXSIZE],
+
+        ["0.0", 0],
+        [0.0, 0],
+
+        ["aaaaa", "aaaaa"],
+
+        [inf, inf],
+    ])
+    def test_normal(self, value, expected):
+        assert convert_value(value) == expected
+
+    @pytest.mark.parametrize(["value", "none_return_value", "expected"], [
+        [None, None, None],
+        ["1", None, 1],
+        [None, "null", "null"],
+        ["1", "null", 1],
+    ])
+    def test_none(self, value, none_return_value, expected):
+        assert convert_value(value, none_return_value) == expected
+
+    def test_abnormal(self):
+        assert is_nan(convert_value(nan))
