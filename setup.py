c9be62cfd8d39f263cff494cfdc3c764bed01adc
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-10-31 00:19:28 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ project_name = "DataProperty"
 
 setuptools.setup(
     name=project_name,
-    version="0.10.0",
+    version="0.10.1",
     url="https://github.com/thombashi/" + project_name,
     bugtrack_url="https://github.com/thombashi/{:s}/issues".format(
         project_name),
