e961722723033ab1901ec6739eea83a0c0adf7ac
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-01-02 19:59:44 +0900

    [ci skip] Update examples

---
+++ examples/py/to_col_dataproperty_list.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import print_function
+import sys
+
+import datetime
+from dataproperty import DataPropertyExtractor, Typecode
+
+
+def display_col_dp(dp_list, attr_name):
+    """
+    show a value assocciated with an attribute for each
+    DataProperty instance in the dp_list
+    """
+
+    print()
+    print("---------- {:s} ----------".format(attr_name))
+    print([getattr(dp, attr_name) for dp in dp_list])
+
+
+def main():
+    # sample data definitions
+    dt = datetime.datetime(2017, 1, 1, 0, 0, 0)
+    inf = float("inf")
+    nan = float("nan")
+    data_matrix = [
+        [1, 1.1,  "aa",   1,   1,     True,   inf,   nan,   dt],
+        [2, 2.2,  "bbb",  2.2, 2.2,   False,  "inf", "nan", dt],
+        [3, 3.33, "cccc", -3,  "ccc", "true", inf,
+            "NAN", "2017-01-01T01:23:45+0900"],
+    ]
+
+    # extract property for each column from a matrix
+    dp_extractor = DataPropertyExtractor()
+    dp_extractor.header_list = [
+        "int", "float", "str", "num", "mix", "bool", "inf", "nan", "time"]
+    dp_extractor.data_matrix = data_matrix
+    col_dp_list = dp_extractor.to_col_dataproperty_list()
+
+    print("---------- typename ----------")
+    print([Typecode.get_typename(dp.typecode) for dp in col_dp_list])
+
+    display_col_dp(col_dp_list, "align")
+    display_col_dp(col_dp_list, "ascii_char_width")
+    display_col_dp(col_dp_list, "decimal_places")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ examples/py/to_dataproperty_matrix.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import print_function
+import sys
+
+import datetime
+from dataproperty import DataPropertyExtractor, Typecode
+
+
+def display_dp_matrix_attr(dp_matrix, attr_name):
+    """
+    show a value assocciated with an attribute for each
+    DataProperty instance in the dp_matrix
+    """
+
+    print()
+    print("---------- {:s} ----------".format(attr_name))
+    for dp_list in dp_matrix:
+        print([getattr(dp, attr_name) for dp in dp_list])
+
+
+def main():
+    # sample data definitions
+    dt = datetime.datetime(2017, 1, 1, 0, 0, 0)
+    inf = float("inf")
+    nan = float("nan")
+
+    # extract data property for each element from a matrix
+    dp_extractor = DataPropertyExtractor()
+    dp_extractor.data_matrix = [
+        [1, 1.1,  "aa",   1,   1,     True,   inf,   nan,   dt],
+        [2, 2.2,  "bbb",  2.2, 2.2,   False,  "inf", "nan", dt],
+        [3, 3.33, "cccc", -3,  "ccc", "true", inf,
+            "NAN", "2017-01-01T01:23:45+0900"],
+    ]
+    dp_matrix = dp_extractor.to_dataproperty_matrix()
+
+    print("---------- typename ----------")
+    for dp_list in dp_matrix:
+        print([Typecode.get_typename(dp.typecode) for dp in dp_list])
+
+    display_dp_matrix_attr(dp_matrix, "data")
+    display_dp_matrix_attr(dp_matrix, "align")
+    display_dp_matrix_attr(dp_matrix, "str_len")
+    display_dp_matrix_attr(dp_matrix, "integer_digits")
+    display_dp_matrix_attr(dp_matrix, "decimal_places")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
