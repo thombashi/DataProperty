6499651394001923c350cd4aaa7c02a0a2371b80
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-11-04 16:39:58 +0900

    Bump version to 0.26.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ EMAIL = "tsuyoshi.hombashi@gmail.com"
 
 setuptools.setup(
     name=MODULE_NAME,
-    version="0.26.0",
+    version="0.26.1",
     url="https://github.com/thombashi/{}".format(MODULE_NAME),
 
     author=AUTHOR,
