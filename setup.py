c62458b6df3138837502c7331111f9acacba1045
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-22 10:08:45 +0900

    Update extras_require

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setuptools.setup(
     extras_require={
         "build": "wheel",
         "docs": docs_requires,
-        "release": "releasecmd>=0.0.8",
+        "release": "releasecmd>=0.0.9",
         "test": tests_requires,
     },
 
