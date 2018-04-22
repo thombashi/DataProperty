84fe3867b92f37d5c8cd8a55dcbf314958c5d682
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-22 19:39:55 +0900

    Apply isort

--- dataproperty/__init__.py
+++ dataproperty/__init__.py
@@ -9,28 +9,10 @@ from __future__ import absolute_import
 from .__version__ import __author__, __copyright__, __email__, __license__, __version__
 from ._align import Align
 from ._align_getter import align_getter
-from ._common import (
-    NOT_QUOTING_FLAGS,
-    NOT_STRICT_TYPE_MAPPING,
-    STRICT_TYPE_MAPPING,
-    DefaultValue,
-)
+from ._common import NOT_QUOTING_FLAGS, NOT_STRICT_TYPE_MAPPING, STRICT_TYPE_MAPPING, DefaultValue
 from ._container import MinMaxContainer
-from ._dataproperty import (
-    ColumnDataProperty,
-    DataProperty,
-)
-from ._extractor import (
-    DataPropertyExtractor,
-    MatrixFormatting,
-)
+from ._dataproperty import ColumnDataProperty, DataProperty
+from ._extractor import DataPropertyExtractor, MatrixFormatting
 from ._function import (
-    is_multibyte_str,
-    get_integer_digit,
-    get_number_of_digit,
-    get_ascii_char_width,
-)
-from ._logger import (
-    set_logger,
-    set_log_level,
-)
+    get_ascii_char_width, get_integer_digit, get_number_of_digit, is_multibyte_str)
+from ._logger import set_log_level, set_logger
