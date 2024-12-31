519f605ee2dbed2c04bd0ec01217ba0d521431b6
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-12-31 23:34:31 +0900

    Bump version to 1.1.0
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- dataproperty/__version__.py
+++ dataproperty/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "Tsuyoshi Hombashi"
 __copyright__: Final = f"Copyright 2016-2024, {__author__}"
 __license__: Final = "MIT License"
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 __maintainer__: Final = __author__
 __email__: Final = "tsuyoshi.hombashi@gmail.com"
