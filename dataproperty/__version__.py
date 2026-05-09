da381f606e06572d990cd22f69fb02ae6a7c1892
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2026-05-09 19:30:41 +0900

    Update copyright year
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- dataproperty/__version__.py
+++ dataproperty/__version__.py
@@ -2,7 +2,7 @@ from typing import Final
 
 
 __author__: Final = "Tsuyoshi Hombashi"
-__copyright__: Final = f"Copyright 2016-2024, {__author__}"
+__copyright__: Final = f"Copyright 2016-2026, {__author__}"
 __license__: Final = "MIT License"
 __version__ = "1.1.0"
 __maintainer__: Final = __author__
